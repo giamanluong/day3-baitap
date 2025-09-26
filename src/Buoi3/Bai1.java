a0dc13730f760ad078f93f0852b96ec72e27ef02
LuongGiaMan <giamanluong2104@gmail.com>
2025-09-26 20:38:47 +0700

    bai tap java buoi 3

---
+++ src/Buoi3/Bai1.java
@@ -0,0 +1,40 @@
+package Buoi3;
+
+import java.time.DateTimeException;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        /*int x=7;
+        int y=x++;//lấy giá trị x in ra rồi tăng x lên 8
+        int a=++x;//tăng từ 8 lên 9 rồi mới in ra
+        System.out.println(y);
+        System.out.println(a);*/
+        /*int a= 10;
+        int b=3;
+        System.out.println(a/b);
+        System.out.println(a%b);*/
+        /* int a=5;
+         int b=10;
+         System.out.println(a>b);
+         System.out.println(a<b);
+         System.out.println(a==b);*/
+        /*boolean isRain= true;
+        boolean isSunny=false;
+        System.out.println(isRain && isSunny);
+        System.out.println(isRain || isSunny);
+        System.out.println(isRain != isSunny);*/
+        /*int chieuRong=5;
+        int chieuDai=7;
+        System.out.println(chieuRong*chieuDai);*/
+        String ten="Lương Gia Mẫn";
+        int tuoi=21;
+        String ngaySinh="2004-04-21";
+        String queQuan="Thành phố Huế";
+        String nganhHoc="Công nghệ thông tin";
+        System.out.println(ten);
+        System.out.println(tuoi);
+        System.out.println(ngaySinh);
+        System.out.println(queQuan);
+        System.out.println(nganhHoc);
+    }
+}
