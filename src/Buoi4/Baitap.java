27e80e1b3cb0af16e408ad20774111d723e3ad89
LuongGiaMan <giamanluong2104@gmail.com>
2025-10-03 20:55:03 +0700

    Bai tap buoi 4

---
+++ src/Buoi4/Baitap.java
@@ -0,0 +1,30 @@
+package Buoi4;
+
+public class Baitap {
+    public static void main(String[] args) {
+       /* int a=100;
+        double b = a;
+        System.out.println(b);
+    }*/
+     /*   double x = 9.75;
+        int y = (int) x;
+        System.out.println(y);*/
+        /*int n= 2;
+        double canbachai= Math.sqrt(n);
+        System.out.println(canbachai);
+        double luythua=Math.pow(n,3);
+        System.out.println(luythua);*/
+        /*int a=-15 , b=20;
+        double trituyetdoi= Math.abs(a);
+        System.out.println(trituyetdoi);
+        int giatrilonhon=Math.max(a,b);
+        System.out.println(giatrilonhon);
+        int giatrinhohon=Math.min(a,b);
+        System.out.println(giatrinhohon);*/
+        double y=5.67;
+        double lamtronduoi= Math.floor(y);
+        System.out.println(lamtronduoi);
+        double lamtrontren= Math.ceil(y);
+        System.out.println(lamtrontren);
+    }
+}
